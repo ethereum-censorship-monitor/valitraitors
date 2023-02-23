dbdb88ff2f8d58b11aa830dc5cca183488c9e047
Jannik Luhn <jannik.luhn@posteo.de>
2023-02-23 17:19:02 +0800

    Fix table positioning

--- frontend/src/lib/Table.svelte
+++ frontend/src/lib/Table.svelte
@@ -29,36 +29,38 @@
 </script>
 
 <div class="mx-auto max-w-screen-sm">
-  <table class="table-fixed text-white w-full mx-4 border-b border-white">
-    {#if heads}
-      <thead class="mb-4">
-        <tr>
-          {#each heads as head, i}
-            <th class="w-1/4 align-top pb-2 border-b border-white">
-              <span
-                class="cursor-pointer select-none"
-                class:underline={i === overrideSortColumn}
-                on:click={() => columnClickHandler(i)}>{head}</span
-              >
-            </th>
-          {/each}
-        </tr>
-      </thead>
-    {/if}
-    <tbody>
-      {#each sortedRows as row}
-        <tr>
-          {#each row as cell}
-            <td class="text-center py-1">
-              {#if cell.text != 'Lido'}
-                {cell.text}
-              {:else}
-                Lido <sup>1</sup>
-              {/if}
-            </td>
-          {/each}
-        </tr>
-      {/each}
-    </tbody>
-  </table>
+  <div class="mx-4">
+    <table class="table-fixed text-white w-full border-b border-white">
+      {#if heads}
+        <thead class="mb-4">
+          <tr>
+            {#each heads as head, i}
+              <th class="w-1/4 align-top pb-2 border-b border-white">
+                <span
+                  class="cursor-pointer select-none"
+                  class:underline={i === overrideSortColumn}
+                  on:click={() => columnClickHandler(i)}>{head}</span
+                >
+              </th>
+            {/each}
+          </tr>
+        </thead>
+      {/if}
+      <tbody>
+        {#each sortedRows as row}
+          <tr>
+            {#each row as cell}
+              <td class="text-center py-1">
+                {#if cell.text != 'Lido'}
+                  {cell.text}
+                {:else}
+                  Lido <sup>1</sup>
+                {/if}
+              </td>
+            {/each}
+          </tr>
+        {/each}
+      </tbody>
+    </table>
+  </div>
 </div>
