a713659cd08c5cccc52dc19c9548e2414b14ab48
Jannik Luhn <jannik.luhn@posteo.de>
2023-02-23 18:25:03 +0800

    Fix title underlining in Safari

--- frontend/src/lib/Title.svelte
+++ frontend/src/lib/Title.svelte
@@ -1,5 +1,19 @@
-<h1
-  class="font-heading italic text-6xl md:text-8xl font-bold text-center mt-12 mb-8 text-white underline decoration-8 decoration-cyan-500"
->
-  <a href="/"> Valitraitors </a>
-</h1>
+<script>
+  import { onMount } from 'svelte';
+
+  let isSafari = false;
+
+  onMount(() => {
+    isSafari = window.navigator.userAgent.indexOf('Safari') != -1;
+    console.log(isSafari);
+  });
+</script>
+
+<a href="/">
+  <h1
+    class="font-heading italic text-6xl md:text-8xl font-bold text-center mt-12 mb-8 text-white underline decoration-8 decoration-cyan-500"
+    class:underline-offset-8={isSafari}
+  >
+    Valitraitors
+  </h1>
+</a>
