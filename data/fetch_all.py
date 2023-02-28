3f06472f50e575eaaf3bde3e55564e01660aa3ad
Jannik Luhn <jannik.luhn@posteo.de>
2023-02-28 11:40:38 +0800

    Increase log output

--- data/fetch_all.py
+++ data/fetch_all.py
@@ -10,15 +10,25 @@ import create_lido_leaderboard
 
 
 def main():
+    print("fetching txs...")
     fetch_txs.main()
+    print("fetching blocks...")
     fetch_blocks.main()
+    print("fetching relays...")
     fetch_relays.main()
+    print("fetching validator pubkeys...")
     fetch_validator_pubkeys.main()
+    print("fetching lido...")
     fetch_lido.main()
+    print("creating depositor leaderboard...")
     create_depositor_leaderboard.main()
+    print("creating builder leaderboard...")
     create_builder_leaderboard.main()
+    print("creating relay leaderboard...")
     create_relay_leaderboard.main()
+    print("creating lido leaderboard...")
     create_lido_leaderboard.main()
+    print("done.")
 
 
 if __name__ == "__main__":
